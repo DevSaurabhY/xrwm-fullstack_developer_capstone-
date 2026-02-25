cbcb161b45c95c977ab24ed9ae92493839550d06
Saurabh Yadav <143438180+DevSaurabhY@users.noreply.github.com>
2026-02-25 16:44:04 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('fetchReviews/dealer/<int:dealer_id>', views.fetch_reviews, name='fetch_reviews'),
+    path('fetchDealers', views.fetch_dealers, name='fetch_dealers'),
 ]
